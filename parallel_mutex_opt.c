4c53e42f3715071fc11302ce5360a38649673996
Harindham Sharma <harindhamsharma@Mac.lan>
2025-11-20 01:11:31 -0500

    retrieve operation optimised

--- parallel_mutex_opt.c
+++ parallel_mutex_opt.c
@@ -49,15 +49,12 @@ void insert(int key, int val) {
 // Returns NULL if the key isn't found in the table
 bucket_entry * retrieve(int key) {
   int i = key % NUM_BUCKETS;
-  pthread_mutex_lock(&bucket_locks[i]);
   bucket_entry *b;
   for (b = table[key % NUM_BUCKETS]; b != NULL; b = b->next) {
     if (b->key == key){
-      pthread_mutex_unlock(&bucket_locks[i]);
       return b;
     }
   }
-  pthread_mutex_unlock(&bucket_locks[i]);
   return NULL;
 }
 
